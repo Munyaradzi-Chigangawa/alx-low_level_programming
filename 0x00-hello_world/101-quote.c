f4f47a3517c22ac358f6130201cc35534e3ea5a2
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2023-02-16 17:12:41 -0800

    Advanced Task 2

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - prints exactly "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19",
+ * followed by a new line, to the standard error.
+ * Return: [0] if Successfully executed
+ */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
