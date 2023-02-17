6e07eb1a710a05e173779c27101c158c8704695a
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2023-02-16 16:56:49 -0800

    Task 6

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- *  * main - prints the phrase with proper grammar,
- *   * but the outcome is a piece of art,
- *    * Return: 0 if sucessfully executed
- *     */
+ * main - prints the phrase with proper grammar,
+ * but the outcome is a piece of art,
+ * Return: 0 if sucessfully executed
+ */
 int main(void)
 {
 		printf("with proper grammar, but the outcome is a piece of art,\n");
