12e871c69edc246a8ab5cae84f2199a8d2f833a2
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2023-02-18 02:49:48 -0800

    task 9'

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,13 +5,20 @@ include <stdio.h>
  * followed by a new line
  * Return: 0 (Success)
  */
+
 int main(void)
 {
+	int n;
 	char ch;
 
-	for (ch = 'z'; ch >= 'a'; ch--)
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
 	{
 		putchar(ch);
+
 	}
 	putchar('\n');
 	return (0);
