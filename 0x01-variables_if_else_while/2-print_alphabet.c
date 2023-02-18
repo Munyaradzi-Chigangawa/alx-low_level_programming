4b23568b85b3dc3bf756b43703805055ae348cfd
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2023-02-18 10:49:24 +0200

    Task 3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/*
+ * main - entry point
+ * description - printing the alphabet in lowerCase
+ * return - 0 if successfully executed
+ */
+
+int main() {
+    char ch;
+
+    for (
+            ch = 'a';
+            ch<= 'z';
+            ch++
+            )
+    {
+        putchar(ch);
+    }
+    putchar('\n');
+    return 0;
+}
