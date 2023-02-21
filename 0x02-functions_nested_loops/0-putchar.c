7dd62ed901acb001422cf7dd142b7821296d02f6
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2023-02-21 11:17:59 -0800

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+	/**
+	 * main - Entry Point
+	 * description: using _putchar
+	 * return: 0 (Success)
+	 */
+
+	_putchar(_);
+	_putchar(p);
+	_putchar(u);
+	_putchar(t);
+	_putchar(c);
+	_putchar(h);
+	_putchar(a);
+	_putchar(r);
+
+	printf("/n");
+	return (0);
+}
