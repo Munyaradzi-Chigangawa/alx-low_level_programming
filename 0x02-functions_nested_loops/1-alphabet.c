2506c81fc6bb80fcf140736edfd16fbb599885ad
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2023-02-21 14:04:41 -0800

    task 3

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,6 +5,7 @@
 * description: - print alphabet
 * Return: void
 */
+
 void print_alphabet(void)
 {
 	char letter = 'a';
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - Entry Point
+ * description: print alphabet 10 times
+ * return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int round = 0;
+	char letter = 'a';
+
+	while (round < 10)
+	{
+		letter = 'a';
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		_putchar('\n');
+
+		round++;
+	}
+}
