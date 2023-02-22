b8b88f41d372a66f4c56685657d58d9072af67d6
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2023-02-22 12:22:33 -0800

    task

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,13 +2,14 @@
 
 /**
  * print_last_digit - Entry point
- * @n: return last value
- * return: lastDigit
+ * @n: return val
+ * return: val
  */
 
 int print_last_digit(int i)
 {
 	int val;
+
 	val = i % 10;
 	if (val < 0)
 		val *= -1;
