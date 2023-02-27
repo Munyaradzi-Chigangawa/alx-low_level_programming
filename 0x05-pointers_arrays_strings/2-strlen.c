84772a70b0559affaf647bcc9d73cb44243b46f8
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2023-02-27 02:53:43 -0800

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - return the length of string.
+ * @s: chartype pointer
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int c;
+
+	for (c = 0; s[c] != '\0'; c++);
+	return (c);
+}
